328e1e63ff43ce2cafc820b7053a6e7dfa58c400
dantegr <tampantelis@gmail.com>
2023-11-19 11:44:56 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+import "./App.css";
+
+const getNextWednessday = (date = new Date()) => {
+  const dateCopy = new Date(date.getTime());
+
+  const nextWednessday = new Date(
+    dateCopy.setDate(
+      dateCopy.getDate() + ((7 - dateCopy.getDay() + 3) % 7 || 7)
+    )
+  );
+
+  nextWednessday.setHours(18, 30, 0);
+
+  return nextWednessday;
+};
+
+const Time = (timeNow) => {
+  const countDownDate = getNextWednessday(new Date()).getTime();
+  const now = timeNow.timeNow.getTime();
+  const distance = countDownDate - now;
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor(
+    (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div>
+      <div className="fade"></div>
+      <h1>Χρόνος για το επόμενο session!</h1>
+      <div className="countbox">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {days} <span>Μέρες</span>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p>
+          {hours} <span>Ώρες</span>
+        </p>
+        <p>
+          {minutes} <span>Λεπτά</span>
+        </p>
+        <p>
+          {seconds} <span>Δευτερόλπετα</span>
+        </p>
+      </div>
     </div>
   );
-}
+};
+
+const App = () => {
+  const now = new Date();
+  const [timeNow, setTimeNow] = useState(now);
+  const updatetime = () => {
+    const newtime = new Date();
+    setTimeNow(newtime);
+  };
+  setInterval(updatetime, 1000);
+
+  return (
+    <div className="App">
+      <Time timeNow={timeNow} />
+    </div>
+  );
+};
 
 export default App;
